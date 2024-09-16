0ac10e8c612f0729635fb8f814ea2c67a122a455
Jacob Harris <jacobprestonharris@gmail.com>
2024-09-16 23:34:28 +0000

    just a bunch of cleaning of the code. also the hwaistruction now hides away

--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -1,321 +1,8 @@
-// Reference to the overlay
+// =================== General Setup ===================
+// Reference to the overlay and instructions
 const overlay = document.getElementById("overlay");
 const instructions = document.getElementById("instruction-text");
 const hwainstructions = document.getElementById("hwa");
-
-if (isWebGLSupported()) {
-  hwainstructions.style.display = "none";
-  //3d background render
-  // Set up the scene
-  const scene = new THREE.Scene();
-  scene.background = new THREE.Color(0x87ceeb); // Sky blue background
-
-  // Set up the camera
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
-  const radius = 20;
-  let angle = 0;
-  const baseRotationSpeed = 0.0001; // Base rotation speed
-  let scrollRotationSpeed = 0; // Additional rotation speed from scroll
-  const maxRotationSpeed = 0.01; // Maximum speed limit
-  const dampingFactor = 0.98; // Damping factor for slowing down
-
-  camera.position.set(radius, 10, 0); // Initial position
-  camera.lookAt(0, 0, 0); // Look at the center of the model
-
-  // Set up the renderer
-  const renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
-
-  // Add ambient light
-  const ambientLight = new THREE.AmbientLight(0x404040, 2); // Soft white light
-  scene.add(ambientLight);
-
-  // Add directional light
-  const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-  directionalLight.position.set(50, 50, 50);
-  directionalLight.castShadow = true;
-  scene.add(directionalLight);
-
-  const directionalLight2 = new THREE.DirectionalLight(0xffffff, 0.5);
-  directionalLight2.position.set(-50, 50, -50);
-  directionalLight2.castShadow = true;
-  scene.add(directionalLight2);
-
-  // Load the .glb file
-  const loader = new THREE.GLTFLoader();
-  loader.load(
-    "assets/models/volc.glb",
-    function (gltf) {
-      const object = gltf.scene;
-      object.scale.set(10, 10, 10); // Adjust scale if needed
-      object.position.set(0, 0, 0); // Adjust position if needed
-      scene.add(object);
-    },
-    undefined,
-    function (error) {
-      console.error("An error happened:", error);
-    }
-  );
-
-  // Post-processing
-  const composer = new THREE.EffectComposer(renderer);
-  const renderPass = new THREE.RenderPass(scene, camera);
-  composer.addPass(renderPass);
-
-  const unrealBloomPass = new THREE.UnrealBloomPass(
-    new THREE.Vector2(window.innerWidth, window.innerHeight),
-    0.3,
-    0.1,
-    0.1 // Toned down bloom effect
-  );
-  unrealBloomPass.threshold = 0;
-  unrealBloomPass.strength = 0.3; // Reduced bloom strength
-  unrealBloomPass.radius = 0.1; // Reduced bloom radius
-  composer.addPass(unrealBloomPass);
-
-  const shaderPass = new THREE.ShaderPass(THREE.CopyShader);
-  composer.addPass(shaderPass);
-
-  // Handle scroll event to adjust rotation speed
-  window.addEventListener("wheel", function (event) {
-    scrollRotationSpeed += event.deltaY * 0.0005; // Adjust rotation speed based on scroll
-    if (scrollRotationSpeed > maxRotationSpeed) {
-      scrollRotationSpeed = maxRotationSpeed;
-    } else if (scrollRotationSpeed < -maxRotationSpeed) {
-      scrollRotationSpeed = -maxRotationSpeed;
-    }
-  });
-
-  // Update camera position
-  function updateCameraPosition() {
-    angle += baseRotationSpeed + scrollRotationSpeed; // Update angle based on combined rotation speed
-    camera.position.x = radius * Math.cos(angle);
-    camera.position.z = radius * Math.sin(angle);
-    camera.lookAt(0, 0, 0); // Keep the camera looking at the center of the model
-
-    // Apply damping to scrollRotationSpeed to gradually slow down to base speed
-    scrollRotationSpeed *= dampingFactor;
-    if (Math.abs(scrollRotationSpeed) < 0.0001) {
-      scrollRotationSpeed = 0; // Stop completely when very slow
-    }
-  }
-
-  // Animate the scene
-  function animate() {
-    requestAnimationFrame(animate);
-    updateCameraPosition();
-    composer.render();
-  }
-  animate();
-
-  // Handle window resize
-  window.addEventListener("resize", function () {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    composer.setSize(window.innerWidth, window.innerHeight);
-  });
-
-  // Add event listener to the volcano model
-  loader.load(
-    "assets/models/volc.glb",
-    function (gltf) {
-      if (overlay.style.display == "block") {
-        return;
-      }
-
-      const object = gltf.scene;
-      object.scale.set(10, 10, 10); // Adjust scale if needed
-      object.position.set(0, 0, 0); // Adjust position if needed
-      scene.add(object);
-
-      // Add click event to the object
-      object.traverse((child) => {
-        if (child.isMesh) {
-          child.userData.clickable = true;
-        }
-      });
-
-      const raycaster = new THREE.Raycaster();
-      const mouse = new THREE.Vector2();
-
-      function onClick(event) {
-        mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-        mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-        raycaster.setFromCamera(mouse, camera);
-
-        const intersects = raycaster.intersectObjects(scene.children, true);
-        for (let i = 0; i < intersects.length; i++) {
-          if (intersects[i].object.userData.clickable) {
-            showOverlay();
-          }
-        }
-      }
-
-      function onMouseMove(event) {
-        mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-        mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-        raycaster.setFromCamera(mouse, camera);
-
-        const intersects = raycaster.intersectObjects(scene.children, true);
-        let isHovering = false;
-        for (let i = 0; i < intersects.length; i++) {
-          if (intersects[i].object.userData.clickable) {
-            isHovering = true;
-          }
-        }
-        document.body.style.cursor = isHovering ? "pointer" : "default";
-      }
-
-      window.addEventListener("click", onClick);
-      window.addEventListener("mousemove", onMouseMove);
-    },
-    undefined,
-    function (error) {
-      console.error("An error happened:", error);
-    }
-  );
-} else {
-  hwainstructions.style.display = "block";
-  // If WebGL is not supported, display the fallback volcano image
-  const fallbackImage = document.createElement("img");
-  fallbackImage.src = "assets/imgs/volcano.png"; // Path to the volcano PNG
-  fallbackImage.id = "fallback-volcano";
-  fallbackImage.style.cursor = "pointer";
-
-  // Add click event to the image
-  fallbackImage.addEventListener("click", onClick);
-
-  // Append the image to the same container where the WebGL canvas would go
-  document.body.appendChild(fallbackImage);
-
-  function onClick(event) {
-    console.log("click");
-    showOverlay();
-  }
-}
-
-//music playlist
-const API_KEY = "AIzaSyB58fVAN2Q2P3z3kLwI_KnPX7rs20TTMDA";
-const API_ID =
-  "491927014195-cve61bomorg41tg8k51n9hv4etpjglne.apps.googleusercontent.com";
-const PLAYLIST_ID = "PLZG0CvngYU9i8BQVur9DtUyOGOALdtorK";
-
-let videoVisible = 0;
-let amount = 0;
-let index = 0;
-
-const playlistItems = [];
-const playlist = document.getElementById("playlist");
-
-function initAPIClient() {
-  gapi.load("client:auth2", function () {
-    gapi.auth2.init({ client_id: API_ID });
-    loadClient().then(getPlaylistItems);
-  });
-}
-
-function loadClient() {
-  gapi.client.setApiKey(API_KEY);
-  return gapi.client
-    .load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-    .then(
-      () => console.log("GAPI client loaded for API"),
-      (err) => console.error("Error loading GAPI client", err)
-    );
-}
-
-function getPlaylistItems(pageToken) {
-  return gapi.client.youtube.playlistItems
-    .list({
-      part: "snippet,contentDetails",
-      maxResults: 200,
-      playlistId: PLAYLIST_ID,
-      pageToken,
-    })
-    .then(
-      (response) => {
-        const { items, nextPageToken } = response.result;
-        playlistItems.push(...items);
-        items.forEach((item) => {
-          const thumbnailItem = createPlaylistItem(item, index);
-          playlist.appendChild(thumbnailItem);
-          index++;
-          amount++;
-        });
-        if (nextPageToken) {
-          getPlaylistItems(nextPageToken);
-        }
-      },
-      (err) => console.error("Error fetching playlist items", err)
-    );
-}
-
-function createPlaylistItem(i, index) {
-  const item = document.createElement("div");
-  item.classList.add("playlist-item");
-  item.id = `video-${index}`;
-
-  // Apply the background image (video thumbnail)
-  item.style.backgroundImage = `url(https://i.ytimg.com/vi/${i.snippet.resourceId.videoId}/mqdefault.jpg)`;
-
-  // Create and append the title overlay
-  const title = document.createElement("div");
-  title.classList.add("title");
-  title.textContent = i.snippet.title; // Set the title text
-  item.appendChild(title);
-
-  // On click, navigate to the YouTube video
-  item.addEventListener("click", function () {
-    window.open(
-      `https://www.youtube.com/watch?v=${i.snippet.resourceId.videoId}&list=${PLAYLIST_ID}&index=${index}`,
-      "_blank"
-    );
-  });
-
-  // Append item to the playlist
-  playlist.appendChild(item);
-
-  return item;
-}
-
-// Function to fetch and display all playlist items
-function getPlaylistItems(pageToken) {
-  return gapi.client.youtube.playlistItems
-    .list({
-      part: "snippet,contentDetails",
-      maxResults: 200, // Maximum available
-      playlistId: PLAYLIST_ID,
-      pageToken,
-    })
-    .then(
-      function (response) {
-        const { items, nextPageToken } = response.result;
-        playlistItems.push(...items);
-
-        // Render all playlist items
-        items.forEach(function (item, index) {
-          createPlaylistItem(item, index);
-        });
-
-        // Fetch more items if there are more pages
-        if (nextPageToken) {
-          getPlaylistItems(nextPageToken);
-        }
-      },
-      function (err) {
-        console.error("Execute error", err);
-      }
-    );
-}
-
-initAPIClient();
 
 // Check if WebGL is supported
 function isWebGLSupported() {
@@ -330,29 +17,28 @@ function isWebGLSupported() {
   }
 }
 
-// Existing CSS and 3D model rendering setup...
-
-// Function to show the overlay
+// Show or hide overlay
 function showOverlay() {
   overlay.classList.remove("hidden");
-  overlay.style.display = "block"; // Ensure it is displayed
+  overlay.style.display = "block";
   setTimeout(() => {
     instructions.style.display = "none";
+    hwainstructions.style.display = "none";
     overlay.classList.add("visible");
-  }, 10); // Small delay to trigger the transition
+  }, 10); // Trigger transition after delay
 }
 
-// Function to hide the overlay
 function hideOverlay() {
   overlay.classList.remove("visible");
   overlay.classList.add("hidden");
   setTimeout(() => {
     instructions.style.display = "block";
+    hwainstructions.style.display = "block";
     overlay.style.display = "none";
   }, 500); // Match the transition duration
 }
 
-// Add event listener for the ESC key
+// Add event listener for the ESC key to hide the overlay
 window.addEventListener("keydown", function (event) {
   if (event.key === "Escape") {
     hideOverlay();
@@ -374,14 +60,236 @@ overlay.addEventListener("click", function (event) {
   event.stopPropagation();
 });
 
-playlist.addEventListener("wheel", function (event) {
-  event.preventDefault(); // Prevent the default vertical scrolling behavior
+// =================== 3D Scene Setup (WebGL) ===================
+if (isWebGLSupported()) {
+  hwainstructions.style.display = "none";
 
-  // Scroll horizontally by adjusting the scrollLeft property
-  if (event.deltaY > 0) {
-    playlist.scrollLeft += playlist.offsetWidth * 0.5; // Use vertical scroll value for horizontal movement
+  // 3D Scene variables and setup
+  const scene = new THREE.Scene();
+  scene.background = new THREE.Color(0x87ceeb); // Sky blue background
+
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  // Camera and lighting setup
+  const radius = 20,
+    baseRotationSpeed = 0.0001,
+    maxRotationSpeed = 0.01,
+    dampingFactor = 0.98;
+  let angle = 0,
+    scrollRotationSpeed = 0;
+
+  camera.position.set(radius, 10, 0);
+  camera.lookAt(0, 0, 0);
+  scene.add(new THREE.AmbientLight(0x404040, 2));
+
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+  directionalLight.position.set(50, 50, 50).castShadow = true;
+  scene.add(directionalLight);
+
+  // Post-processing setup
+  const composer = new THREE.EffectComposer(renderer);
+  const renderPass = new THREE.RenderPass(scene, camera);
+  composer.addPass(renderPass);
+
+  const bloomPass = new THREE.UnrealBloomPass(
+    new THREE.Vector2(window.innerWidth, window.innerHeight),
+    0.3,
+    0.1,
+    0.1
+  );
+  bloomPass.threshold = 0;
+  bloomPass.strength = 0.3;
+  bloomPass.radius = 0.1;
+  composer.addPass(bloomPass);
+
+  const shaderPass = new THREE.ShaderPass(THREE.CopyShader);
+  composer.addPass(shaderPass);
+
+  // 3D Model loader
+  const loader = new THREE.GLTFLoader();
+  loader.load(
+    "assets/models/volc.glb",
+    function (gltf) {
+      const object = gltf.scene;
+      object.scale.set(10, 10, 10);
+      scene.add(object);
+    },
+    undefined,
+    function (error) {
+      console.error("An error happened:", error);
+    }
+  );
+
+  // Scroll event handler for rotation speed
+  window.addEventListener("wheel", function (event) {
+    scrollRotationSpeed += event.deltaY * 0.0005;
+    scrollRotationSpeed = Math.max(
+      Math.min(scrollRotationSpeed, maxRotationSpeed),
+      -maxRotationSpeed
+    );
+  });
+
+  // Update camera position
+  function updateCameraPosition() {
+    angle += baseRotationSpeed + scrollRotationSpeed;
+    camera.position.x = radius * Math.cos(angle);
+    camera.position.z = radius * Math.sin(angle);
+    camera.lookAt(0, 0, 0);
+    scrollRotationSpeed *= dampingFactor;
   }
-  if (event.deltaY < 0) {
-    playlist.scrollLeft -= playlist.offsetWidth * 0.5;
+
+  // Animate the scene
+  function animate() {
+    requestAnimationFrame(animate);
+    updateCameraPosition();
+    composer.render();
   }
+  animate();
+
+  // Window resize event handler
+  window.addEventListener("resize", function () {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    composer.setSize(window.innerWidth, window.innerHeight);
+  });
+
+  // Event listener setup for click and hover interactions
+  loader.load("assets/models/volc.glb", function (gltf) {
+    const object = gltf.scene;
+    object.scale.set(10, 10, 10);
+    scene.add(object);
+    setupInteraction(object);
+  });
+
+  function setupInteraction(object) {
+    object.traverse((child) => {
+      if (child.isMesh) child.userData.clickable = true;
+    });
+
+    const raycaster = new THREE.Raycaster();
+    const mouse = new THREE.Vector2();
+
+    function handleMouseMove(event) {
+      mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+      mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+      raycaster.setFromCamera(mouse, camera);
+
+      const intersects = raycaster.intersectObjects(scene.children, true);
+      document.body.style.cursor = intersects.some(
+        (intersect) => intersect.object.userData.clickable
+      )
+        ? "pointer"
+        : "default";
+    }
+
+    function handleClick(event) {
+      mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+      mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+      raycaster.setFromCamera(mouse, camera);
+
+      const intersects = raycaster.intersectObjects(scene.children, true);
+      if (intersects.some((intersect) => intersect.object.userData.clickable)) {
+        showOverlay();
+      }
+    }
+
+    window.addEventListener("mousemove", handleMouseMove);
+    window.addEventListener("click", handleClick);
+  }
+} else {
+  // Fallback for unsupported WebGL
+  hwainstructions.style.display = "block";
+  const fallbackImage = document.createElement("img");
+  fallbackImage.src = "assets/imgs/volcano.png";
+  fallbackImage.id = "fallback-volcano";
+  document.body.appendChild(fallbackImage);
+  fallbackImage.addEventListener("click", showOverlay);
+}
+
+// =================== Playlist Setup ===================
+const API_KEY = "AIzaSyB58fVAN2Q2P3z3kLwI_KnPX7rs20TTMDA";
+const API_ID =
+  "491927014195-cve61bomorg41tg8k51n9hv4etpjglne.apps.googleusercontent.com";
+const PLAYLIST_ID = "PLZG0CvngYU9i8BQVur9DtUyOGOALdtorK";
+
+let index = 0;
+const playlistItems = [];
+const playlist = document.getElementById("playlist");
+
+// Initialize Google API client
+function initAPIClient() {
+  gapi.load("client:auth2", function () {
+    gapi.auth2.init({ client_id: API_ID });
+    loadClient().then(getPlaylistItems);
+  });
+}
+
+function loadClient() {
+  gapi.client.setApiKey(API_KEY);
+  return gapi.client
+    .load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+    .then(
+      () => console.log("GAPI client loaded for API"),
+      (err) => console.error("Error loading GAPI client", err)
+    );
+}
+
+// Fetch playlist items from YouTube API
+function getPlaylistItems(pageToken) {
+  gapi.client.youtube.playlistItems
+    .list({
+      part: "snippet,contentDetails",
+      maxResults: 200,
+      playlistId: PLAYLIST_ID,
+      pageToken,
+    })
+    .then(
+      (response) => {
+        const { items, nextPageToken } = response.result;
+        items.forEach((item) =>
+          playlist.appendChild(createPlaylistItem(item, index++))
+        );
+        if (nextPageToken) getPlaylistItems(nextPageToken);
+      },
+      (err) => console.error("Error fetching playlist items", err)
+    );
+}
+
+// Create playlist item element
+function createPlaylistItem(item, index) {
+  const playlistItem = document.createElement("div");
+  playlistItem.classList.add("playlist-item");
+  playlistItem.style.backgroundImage = `url(https://i.ytimg.com/vi/${item.snippet.resourceId.videoId}/mqdefault.jpg)`;
+
+  const title = document.createElement("div");
+  title.classList.add("title");
+  title.textContent = item.snippet.title;
+  playlistItem.appendChild(title);
+
+  playlistItem.addEventListener("click", () => {
+    window.open(
+      `https://www.youtube.com/watch?v=${item.snippet.resourceId.videoId}&list=${PLAYLIST_ID}&index=${index}`,
+      "_blank"
+    );
+  });
+
+  return playlistItem;
+}
+
+initAPIClient();
+
+// Playlist horizontal scroll
+playlist.addEventListener("wheel", function (event) {
+  event.preventDefault();
+  playlist.scrollLeft +=
+    event.deltaY > 0 ? playlist.offsetWidth * 0.5 : -playlist.offsetWidth * 0.5;
 });
